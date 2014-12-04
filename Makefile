86903fc4607095288146205cbe32e1da9e650210
Dima <dmitry.korolev@gmail.com>
2014-12-03 16:38:04 -1000

    Code.

---
+++ Makefile
@@ -0,0 +1,36 @@
+.PHONY: all indent clean check
+
+CPP=g++
+CPPFLAGS=-std=c++11 -g -Wall
+LDFLAGS=-pthread
+
+PWD=$(shell pwd)
+SRC=$(wildcard *.cc)
+BIN=$(SRC:%.cc=build/%)
+
+all: build ${BIN}
+
+indent:
+	(find . -name "*.cc" ; find . -name "*.h") | xargs clang-format-3.5 -i
+
+clean:
+	rm -rf build
+
+check: build build/CHECK_OK
+
+build/CHECK_OK: build *.h
+	for i in *.h ; do \
+		echo -n $(basename $$i)': ' ; \
+		ln -sf ${PWD}/$$i ${PWD}/build/$$i.cc ; \
+		if [ ! -f build/$$i.h.o -o build/$$i.h.cc -nt build/$$i.h.o ] ; then \
+			${CPP} -I . ${CPPFLAGS} -c build/$$i.cc -o build/$$i.h.o ${LDFLAGS} || exit 1 ; echo 'OK' ; \
+		else \
+			echo 'Already OK' ; \
+		fi \
+	done && echo OK >$@
+
+build:
+	mkdir -p build
+
+build/%: %.cc
+	${CPP} ${CPPFLAGS} -o $@ ${LDFLAGS} $<
---
+++ graph_test_server.cc
@@ -0,0 +1,61 @@
+// When the server is running, use the following command in Ubuntu to see the example chart:
+// curl -s http://localhost:8080/data | grep -e "^            " | sed "s/,//" | graph -T X -a -g 3 -C -W 0.004 -L "Example"
+
+#include <thread>
+
+#include "cereal/include/archives/json.hpp"
+#include "cereal/include/types/vector.hpp"
+#include "cereal/include/types/string.hpp"
+
+#include "network/posix_http_server.h"
+
+const int kPort = 8080;
+
+struct Meta {
+ public:
+  // TODO(sompylasar+dkorolev): Discuss and add more sophisticated parameters, ex., ticks, labels, grid, etc.
+  std::string data_url = "http://localhost:8080/data";
+  std::string line_color = "blue";
+  double line_width = 3.5;
+  template <class A>
+  void serialize(A& ar) {
+    ar(CEREAL_NVP(data_url), CEREAL_NVP(line_color), CEREAL_NVP(line_width));
+  }
+};
+
+struct Data {
+ public:
+  // TODO(sompylasar+dkorolev): Discuss whether value-only is good, or shall we pass in { x, y } pairs.
+  // TODO(sompylasar+dkorolev): Discuss whether any metadata should be returned as well.
+  std::vector<double> data;
+  template <class A>
+  void serialize(A& ar) {
+    ar(CEREAL_NVP(data));
+  }
+};
+
+int main() {
+  Socket s(kPort);
+  while (true) {
+    std::thread([](HTTPConnection c) {
+                  if (c.Method() == "GET" && (c.URL() == "/meta" || c.URL() == "/meta/")) {
+                    // TODO(dkorolev+sompylasar): Discuss multiple URLs for various scales.
+                    std::ostringstream os;
+                    (cereal::JSONOutputArchive(os))(Meta());
+                    c.SendHTTPResponse(os.str(), HTTPResponseCode::OK);
+                  } else if (c.Method() == "GET" && (c.URL() == "/data" || c.URL() == "/data/")) {
+                    // TODO(dkorolev): Add URL parsing to support partial data.
+                    Data data;
+                    for (int i = 0; i < 1000; ++i) {
+                      data.data.push_back(sin(0.01 * i) + sin(0.04 * i) + exp(0.001 * i));
+                    }
+                    std::ostringstream os;
+                    (cereal::JSONOutputArchive(os))(data);
+                    c.SendHTTPResponse(os.str(), HTTPResponseCode::OK);
+                  } else {
+                    c.SendHTTPResponse("Something's wrong.", HTTPResponseCode::NotFound);
+                  }
+                },
+                s.Accept()).detach();
+  }
+}
